2d6e7bfb6bcfacbda5f321bd92257da3bd9c503b
Unknown <dominique.sydow@posteo.de>
2021-05-26 22:41:16 +0200

    Update 2D view signatures

--- dynophores/viz/view2d/interactive.py
+++ dynophores/viz/view2d/interactive.py
@@ -8,15 +8,13 @@ import ipywidgets as widgets
 from dynophores.viz import view2d
 
 
-def show(dynophore, pdb_path):
+def show(dynophore):
     """
     Show interactive ligand 2D view.
     Toogle between superfeatures on/off and atom serial numbers on/off.
 
     Parameters
     ----------
-    pdb_path : str or pathlib.Path
-        Path to PDB file
     dynophore : dynophores.Dynophore
         Dynophore object.
 
@@ -29,7 +27,6 @@ def show(dynophore, pdb_path):
     func = interact(
         view2d.static.show,
         dynophore=fixed(dynophore),
-        pdb_path=fixed(pdb_path),
         show_superfeatures=widgets.Checkbox(value=False, description="Show superfeatures"),
         show_pdb_serial_numbers=widgets.Checkbox(
             value=False, description="Show atom serial numbers"
